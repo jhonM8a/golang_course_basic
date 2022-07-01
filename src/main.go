fe2505f9d1edfcbde0d3ca9c90f0b32a0e350fd9
jhonM8a <jhon.ochoa@unillanos.edu.co>
2022-06-30 19:29:44 -0500

    Operadores arimeticos

--- src/main.go
+++ src/main.go
@@ -3,27 +3,40 @@ package main //Indica la carpeta donde esta, pero como no hay ponemos el main
 import "fmt" //	Libreria para imprimir
 
 func main() {
-	// Delcaración de constantes
-	const pi float64 = 3.14
-	const pi2 = 3.15
-	fmt.Println("pi:", pi)
-	fmt.Println("pi2:", pi2)
-
-	//Delacación de variables entera: Si las variables no se usan dará erros al ejecutar o compilar
-	base := 12 // los dos puntos indican que es una variable que no ha sido delcarada anteriormete
-	var altura int = 14
-	var area int
-	fmt.Println(base, altura, area)
-
-	//Zero values:Es un valor que tiene por defecto al crear una variable y no se le asigna un valor
-	var a int     // valor por defecto 0
-	var b float64 // 0
-	var c string  // valor por decto no nulo, como un string vacio
-	var d bool    //false
-	fmt.Println(a, b, c, d)
-
 	// Area de un cuadrado
 	const baseCuadrado = 10
 	areaCuadrado := baseCuadrado * baseCuadrado // Go asigna el tipo de esta manera
 	fmt.Println("Area cuadrado:", areaCuadrado)
+
+	x := 10
+	y := 50
+
+	// Suma
+	result := x + y // La variable no ha sido definida
+	fmt.Println("Suma:", result)
+
+	// Resta
+	result = y - x
+	fmt.Println("Resta:", result)
+
+	// Multiplicación
+	result = x * y
+	fmt.Println("Multiplicación:", result)
+
+	//División
+	result = y * x
+	fmt.Println("División:", result)
+
+	// Modulo
+	result = y % x
+	fmt.Println("Modulo:", result)
+
+	//Incremental
+	x++
+	fmt.Println("Incremental:", x)
+
+	//Decremental
+	x--
+	fmt.Println("Decremental:", x)
+
 }
