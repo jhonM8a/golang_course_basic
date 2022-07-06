fdad564555ccc30b4d06637d5296d0faf8060120
jhonM8a <jhon.ochoa@unillanos.edu.co>
2022-07-06 17:00:46 -0500

    switch: sin condición

--- src/main.go
+++ src/main.go
@@ -4,12 +4,22 @@ import "fmt"
 //	Libreria para imprimir
 
 func main() {
-	//switch nomal
-	modulo := 4 % 2
-	switch modulo {
+	//switch como se encuentra generalmente
+	switch modulo := 4 % 2; modulo {
 	case 0:
 		fmt.Println("Par")
 	default:
 		fmt.Println("impar")
+	}
+
+	//switch sin condición
+	value := 200
+	switch {
+	case value > 100:
+		fmt.Println("Menor a 100")
+	case value < 0:
+		fmt.Println("Menor a 0")
+	default:
+		fmt.Println("Sin condición")
 	}
 }
