1511543e4833606ba4444cafbbf86b3d956a0221
jhonM8a <jhon.ochoa@unillanos.edu.co>
2022-07-06 16:17:44 -0500

    IF: Uso de if + uso de and y or

--- src/main.go
+++ src/main.go
@@ -1,26 +1,57 @@
 package main //Indica la carpeta donde esta, pero como no hay ponemos el main
 
-import "fmt" //	Libreria para imprimir
+import (
+	"fmt" //	Libreria para imprimir
+	"log"
+	"strconv"
+)
+
+func ValidarParOImpar(number int) {
+	if number%2 == 0 {
+		fmt.Println("El número es par")
+	} else {
+		fmt.Println("El número es impar")
+	}
+
+}
+
+func ValidateUserPassword(user, password string) bool {
+	if user == "jochoa" && password == "test123" {
+		return true
+	} else {
+		return false
+	}
+}
 
 func main() {
-	// For condicional
-	for i := 0; i <= 10; i++ {
-		fmt.Println(i)
+	valor1 := 1
+	valor2 := 2
+	if valor1 == 1 {
+		fmt.Println("Es 1")
+	} else {
+		fmt.Println("No es 1")
 	}
 
-	fmt.Println("-------")
-	// For while
-	counter := 0
-	for counter < 10 {
-		fmt.Println(counter)
-		counter++
-	}
-	fmt.Println("-------")
-	// for forever
-	counterForever := 0
-	for {
-		fmt.Println(counterForever)
-		counterForever++
+	if valor1 == 1 && valor2 == 2 {
+		fmt.Println("Es verdad")
 	}
 
+	if valor1 == 0 || valor2 == 2 {
+		fmt.Println("Es verdad")
+	}
+
+	// Text to number
+	value, err := strconv.Atoi("53")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Value: ", value)
+
+	//Reto 1: Ver si un número es par o impar
+	ValidarParOImpar(3)
+	//Reto 2: Validar usuario y contraseña
+	userValid := ValidateUserPassword("jochoa", "test123")
+	if userValid {
+		fmt.Println("Usuario valido")
+	}
 }
