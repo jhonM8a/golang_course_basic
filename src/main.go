318c620190cf02f417f9b2bc024ef1ae8a0d8440
jhonM8a <jhon.ochoa@unillanos.edu.co>
2022-07-06 15:57:33 -0500

    Ciclo for: En go solo hay un ciclo y es l for

--- src/main.go
+++ src/main.go
@@ -2,28 +2,25 @@ package main //Indica la carpeta donde esta, pero como no hay ponemos el main
 
 import "fmt" //	Libreria para imprimir
 
-func normalFunction(message string) {
-	fmt.Println(message)
-}
-
-func tripleArgument(a int, b int, c string) {
-	fmt.Println(a, b, c)
-}
-
-func returnValue(a int) int {
-	return a * 2
-}
-
-func doubleReturn(a int) (c, d int) {
-	return a, a * 2
-}
-
 func main() {
-	normalFunction("Hola mundo")
-	tripleArgument(1, 2, "Jhon")
-	value := returnValue(3)
-	fmt.Println("value: ", value)
-	value1, value2 := doubleReturn(2) // usando _ le decimos a go que no nos interesa la variable que se retorna
-	fmt.Println("value1, value2: ", value1, value2)
+	// For condicional
+	for i := 0; i <= 10; i++ {
+		fmt.Println(i)
+	}
+
+	fmt.Println("-------")
+	// For while
+	counter := 0
+	for counter < 10 {
+		fmt.Println(counter)
+		counter++
+	}
+	fmt.Println("-------")
+	// for forever
+	counterForever := 0
+	for {
+		fmt.Println(counterForever)
+		counterForever++
+	}
 
 }
