2f9819977ebed2a2110f1935aeea0ced46c02c37
jhonM8a <jhon.ochoa@unillanos.edu.co>
2022-07-09 14:11:41 -0500

    Map, llave valor

--- src/main.go
+++ src/main.go
@@ -1,29 +1,28 @@
 package main //Indica la carpeta donde esta, pero como no hay ponemos el main
-import (
-	"fmt" //	Libreria para imprimir
-	"strings"
-)
-
-func isPalindromo(text string) {
-	var textRevers string
-	for i := len(text) - 1; i >= 0; i-- {
-		textRevers += string(text[i])
-	}
-
-	if strings.ToLower(text) == strings.ToLower(textRevers) {
-		fmt.Println("Es palindromo")
-	} else {
-		fmt.Println("No es palindromo")
-	}
-}
+import "fmt"
 
 func main() {
-	slice := []string{"Hola", "que", "hace"}
+	/**
+	Nota: Los mapas son efectivos cuando tenemos dos o mas listas que se relacionan entre ellas,
+	ya que con los maps tenemos acceso mediante llave valor y son más eficientes que manejar
+	slices o arrays ya que de forma nativa el map implementa cocurrencia para poder interactuar
+	entre las opeaciones que usemos con el map
+	*/
+	m := make(map[string]int) // Asi se delcara un mapa que tiene como llave un string y valor un int
+	m["jose"] = 14
+	m["pepito"] = 30
+	fmt.Println(m)
 
-	for i, valor := range slice {
+	// Recorrer un map
+	for i, valor := range m { // No imprime en un orden en especifico ya que cuando se añaden datos se hacen de manera concurrente
 		fmt.Println(i, valor)
 	}
 
-	isPalindromo("Ama")
+	//Get un valor
+	value := m["jose"] // Si no existe la llave te devuelve el mismo valor que tiene zero value, en este caso como es int un 0
+	fmt.Println(value)
 
+	//Get un valor con ok
+	value2, ok := m["jose"] // El ok indica si la llave existe o no el diccionario
+	fmt.Println(value2, ok)
 }
