8f27e7df725c04fe26935a8e7039acffe146cf27
jhonM8a <jhon.ochoa@unillanos.edu.co>
2022-07-11 19:34:49 -0500

    Struct: Modificadores de acceso

--- src/main.go
+++ src/main.go
@@ -1,18 +1,25 @@
 package main //Indica la carpeta donde esta, pero como no hay ponemos el main
-import "fmt"
-
-type car struct {
-	brand string
-	year  int
-}
+import (
+	pk "curso_golang_platzi/src/mypackage" // El pk es un alias
+	"fmt"
+)
 
 func main() {
-	myCar := car{brand: "Ford", year: 2022} // Forma de instanciar
+	var myCar pk.CarPublic // Si el estruct empieza con mayuscula es publico en minuscula privado
+	myCar.Brand = "Ferrai"
+	myCar.Year = 2022
 	fmt.Println(myCar)
 
-	//Otra forma de instaciar
-	var otherCar car
-	otherCar.brand = "Ferrary"
-	fmt.Println(otherCar)
+	/*
+		Como el struct empieza con minuscula es privado
+		var myOtherCar pk.carPrivate
+		fmt.Println(carPrivate)
+	*/
 
+	pk.PrintMessage() //Como comienza con Mayuscula la función es publica
+	/*
+		A tener en cuenta, si sale el error "is not in GOROOT" con el comando "go mod init" se puede
+		solucionar
+
+	*/
 }
---
+++ src/mypackage/mypackage.go
@@ -0,0 +1,19 @@
+package mypackage
+
+import "fmt"
+
+// CarPublic Car con acceso publico
+type CarPublic struct {
+	Brand string
+	Year  int
+}
+
+type carPrivate struct {
+	brand string
+	year  int
+}
+
+// PrintMessage imprimir un mensaje
+func PrintMessage() {
+	fmt.Println("Hola")
+}
