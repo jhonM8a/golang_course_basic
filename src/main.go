95224f9e056f0b91251c20bbbc8a9092e7a1563f
jhonM8a <jhon.ochoa@unillanos.edu.co>
2022-07-11 19:01:48 -0500

    Struct

--- src/main.go
+++ src/main.go
@@ -1,28 +1,18 @@
 package main //Indica la carpeta donde esta, pero como no hay ponemos el main
 import "fmt"
 
+type car struct {
+	brand string
+	year  int
+}
+
 func main() {
-	/**
-	Nota: Los mapas son efectivos cuando tenemos dos o mas listas que se relacionan entre ellas,
-	ya que con los maps tenemos acceso mediante llave valor y son más eficientes que manejar
-	slices o arrays ya que de forma nativa el map implementa cocurrencia para poder interactuar
-	entre las opeaciones que usemos con el map
-	*/
-	m := make(map[string]int) // Asi se delcara un mapa que tiene como llave un string y valor un int
-	m["jose"] = 14
-	m["pepito"] = 30
-	fmt.Println(m)
+	myCar := car{brand: "Ford", year: 2022} // Forma de instanciar
+	fmt.Println(myCar)
 
-	// Recorrer un map
-	for i, valor := range m { // No imprime en un orden en especifico ya que cuando se añaden datos se hacen de manera concurrente
-		fmt.Println(i, valor)
-	}
+	//Otra forma de instaciar
+	var otherCar car
+	otherCar.brand = "Ferrary"
+	fmt.Println(otherCar)
 
-	//Get un valor
-	value := m["jose"] // Si no existe la llave te devuelve el mismo valor que tiene zero value, en este caso como es int un 0
-	fmt.Println(value)
-
-	//Get un valor con ok
-	value2, ok := m["jose"] // El ok indica si la llave existe o no el diccionario
-	fmt.Println(value2, ok)
 }
