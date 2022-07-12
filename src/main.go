1f0d490b1b9b4eabf03241872ae7fd7ed3c0ec8e
jhonM8a <jhon.ochoa@unillanos.edu.co>
2022-07-12 18:18:44 -0500

    Stringers: personalizar el output de Structs

--- src/main.go
+++ src/main.go
@@ -7,32 +7,16 @@ type pc struct {
 	brand string
 }
 
-func (myPc pc) ping() { // De esta manera podemos añadirle funciones a nuestro struc
-	fmt.Println(myPc.brand, "pong")
-}
+/**
+La estructura de datos " Struct " tiene un método llamado " String " , que podemos sobrescribir para personalizar la salida a consola de los datos del struct.
 
-func (myPc *pc) duplicateRam() { // Se añade la función al struct y además accedmos al valor del struct que esta la posición de memoria
-	myPc.ram = myPc.ram * 2
-}
+*/
+func (myPc pc) String() string {
+	return fmt.Sprintf("Tengo %d GB RAM, %D GB Disco y es una %s", myPc.ram, myPc.disk, myPc.brand)
 
+}
 func main() {
-	a := 50
-	b := &a // & accedemos a la posicíon de memoria de la variable a
-
-	fmt.Println(a, b)
-	fmt.Println(a, *b) // El * es para acceder al valor de la variable que se encuentra en memoria
-
-	*b = 100       // Se modifca la posición de memoria que es la misma para a y b
-	fmt.Println(a) // Al imprimir a queda con otro valor a pesar de que no modificamos directamente la variable pero si la posición de memoria
-
-	myPc := pc{ram: 16, disk: 200, brand: "msi"}
-	fmt.Println(myPc)
-
-	myPc.ping()
+	myPc := pc{ram: 16, disk: 100, brand: "msi"}
 
 	fmt.Println(myPc)
-
-	myPc.duplicateRam()
-	fmt.Println(myPc)
-
 }
