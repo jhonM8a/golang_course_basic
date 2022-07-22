d08f6412f178c8659ff57591523074d6a2705ba5
jhonM8a <jhon.ochoa@unillanos.edu.co>
2022-07-21 19:37:42 -0500

    Gorutimes waitgroup

--- src/main.go
+++ src/main.go
@@ -1,27 +1,22 @@
 package main //Indica la carpeta donde esta, pero como no hay ponemos el main
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func say(text string) {
+func say(text string, wg *sync.WaitGroup) {
+	defer wg.Done() //Esta linea se va a ejecutar hasta el final de la funcion, y de esta forma libera el gorutine del WaitGroup
 	fmt.Println(text)
 }
 
 func main() { //La función main corre en una goruntime
 
-	say("Hello")
-	/*
-		Con la palabra go indica que la función corre en una gorutime, es decir de manera cocurrente.
-		Que va a pasar, se va a imprimir la palabra Hello pero el World no. Esto, debido a que
-		el main acaba su ejecución y mure la gorutime, El World se ejecuta en otro hilo diferente
-		al del main.
-	*/
-	go say("World")
+	var wg sync.WaitGroup //El paquete sync permite interacturar de forma primitiva con las gorutine. Variable que acomula un conjunto de gorutines y los va liberando poco a poco
 
-	/*
-		De esta manera se puede hacer que se imprima el world en el mismo hiilo del main, pero no es recomendable
-	*/
-	time.Sleep(time.Second * 1)
+	fmt.Println("Hello") //say("Hello")
+	wg.Add(1)            //Indicamos que vamos a agregar 1 Gorutine al WaitGroup para que espere su ejecucion antes de que la gurutine base (main) muera, y así le de tiempo a la siguiente gorutine de ejecutarse
 
+	go say("World", &wg)
+
+	wg.Wait() // Funcion del WaitGroup que sirve para decirle al gorutine principal (main) que espere hasta que todas las gorutine del WaitGroup finalicen, es decir, hasta que se ejecute 'defer wg.Done()' en cada una de las goroutines
 }
