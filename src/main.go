df9434766747481abf474a939132cbfb95c191c1
jhonM8a <jhon.ochoa@unillanos.edu.co>
2022-07-22 19:13:28 -0500

    Channels

--- src/main.go
+++ src/main.go
@@ -1,22 +1,16 @@
 package main //Indica la carpeta donde esta, pero como no hay ponemos el main
-import (
-	"fmt"
-	"sync"
-)
+import "fmt"
 
-func say(text string, wg *sync.WaitGroup) {
-	defer wg.Done() //Esta linea se va a ejecutar hasta el final de la funcion, y de esta forma libera el gorutine del WaitGroup
-	fmt.Println(text)
+func say(text string, c chan<- string) { //Es de buena practia indicar el tipo de canal que estamos recibiendo, si es de entrada o salida ya q mejora la eficiencia del código
+	c <- text // Le pasamos al canal el texto
 }
 
-func main() { //La función main corre en una goruntime
+func main() {
+	c := make(chan string, 1) //Con esto se crea el channel que recibira una gorutime que trabajara con string
+	fmt.Println("Hello")
 
-	var wg sync.WaitGroup //El paquete sync permite interacturar de forma primitiva con las gorutine. Variable que acomula un conjunto de gorutines y los va liberando poco a poco
+	go say("Bye", c)
 
-	fmt.Println("Hello") //say("Hello")
-	wg.Add(1)            //Indicamos que vamos a agregar 1 Gorutine al WaitGroup para que espere su ejecucion antes de que la gurutine base (main) muera, y así le de tiempo a la siguiente gorutine de ejecutarse
+	fmt.Println(<-c) // Sacamos el dato que tiene el canal
 
-	go say("World", &wg)
-
-	wg.Wait() // Funcion del WaitGroup que sirve para decirle al gorutine principal (main) que espere hasta que todas las gorutine del WaitGroup finalicen, es decir, hasta que se ejecute 'defer wg.Done()' en cada una de las goroutines
 }
