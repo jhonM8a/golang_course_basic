b3f7948fb3119a5c9dbff0f96b9f5075bf9d2649
jhonM8a <jhon.ochoa@unillanos.edu.co>
2022-07-22 19:32:21 -0500

    Range, close y select en channels

--- src/main.go
+++ src/main.go
@@ -1,16 +1,39 @@
 package main //Indica la carpeta donde esta, pero como no hay ponemos el main
 import "fmt"
 
-func say(text string, c chan<- string) { //Es de buena practia indicar el tipo de canal que estamos recibiendo, si es de entrada o salida ya q mejora la eficiencia del código
-	c <- text // Le pasamos al canal el texto
+func message(text string, c chan string) {
+	c <- text
 }
 
 func main() {
-	c := make(chan string, 1) //Con esto se crea el channel que recibira una gorutime que trabajara con string
-	fmt.Println("Hello")
+	c := make(chan string, 2)
+	c <- "Mensaje1"
+	c <- "Mensaje2"
 
-	go say("Bye", c)
+	fmt.Println(len(c), cap(c)) //len --> Cantidad de gorutimes que hay en el channel y cap --> capacidad maxima de gorutimes que puede almacenar el channel
 
-	fmt.Println(<-c) // Sacamos el dato que tiene el canal
+	// Close
+	// Close le indica al runtime de go que ese canal no va a recibir ni un dato más, auque tenga la capacidad para hacerlo.
+	close(c)
 
+	// Range
+	// Range ayuda a recorrer todos los mensajes que hay en ese channel
+	for message := range c {
+		fmt.Println(message)
+	}
+
+	// Select
+	// Select nos ayuda cuando manejamos multiples canales y no sabemos cual va responde primero
+	email1 := make(chan string)
+	email2 := make(chan string)
+	go message("mensaje1", email1)
+	go message("mensaje2", email2)
+	for i := 0; i < 2; i++ { //Es necesario tener presente cuantos channel y mensajes vayamos a tener
+		select {
+		case m1 := <-email1:
+			fmt.Println("Email recibido de email1:", m1)
+		case m2 := <-email2:
+			fmt.Println("Email recibido de email2:", m2)
+		}
+	}
 }
