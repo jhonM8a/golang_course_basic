c77bd4818f25e868d5e5c7640a989b4385e96f5f
jhonM8a <jhon.ochoa@unillanos.edu.co>
2022-07-25 17:13:04 -0500

    Go modules: Using 3rd library

--- src/main.go
+++ src/main.go
@@ -1,39 +1,19 @@
 package main //Indica la carpeta donde esta, pero como no hay ponemos el main
-import "fmt"
+import (
+	"net/http"
 
-func message(text string, c chan string) {
-	c <- text
-}
+	"github.com/labstack/echo"
+)
 
 func main() {
-	c := make(chan string, 2)
-	c <- "Mensaje1"
-	c <- "Mensaje2"
+	//Instanciar echo
+	e := echo.New()
 
-	fmt.Println(len(c), cap(c)) //len --> Cantidad de gorutimes que hay en el channel y cap --> capacidad maxima de gorutimes que puede almacenar el channel
+	//Ruta
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hola mundo")
+	})
 
-	// Close
-	// Close le indica al runtime de go que ese canal no va a recibir ni un dato más, auque tenga la capacidad para hacerlo.
-	close(c)
+	e.Logger.Fatal(e.Start(":1323"))
 
-	// Range
-	// Range ayuda a recorrer todos los mensajes que hay en ese channel
-	for message := range c {
-		fmt.Println(message)
-	}
-
-	// Select
-	// Select nos ayuda cuando manejamos multiples canales y no sabemos cual va responde primero
-	email1 := make(chan string)
-	email2 := make(chan string)
-	go message("mensaje1", email1)
-	go message("mensaje2", email2)
-	for i := 0; i < 2; i++ { //Es necesario tener presente cuantos channel y mensajes vayamos a tener
-		select {
-		case m1 := <-email1:
-			fmt.Println("Email recibido de email1:", m1)
-		case m2 := <-email2:
-			fmt.Println("Email recibido de email2:", m2)
-		}
-	}
 }
